67e0a015a41ce8208f1adf78cb94f1183db4279e
picyoh <picotyoh@gmail.com>
2022-02-11 12:27:27 +0100

    feat: Modal
    adding modal component

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,10 +1,46 @@
 import React from 'react'
 
+import Modal from 'react-modal'
+
+const customStyles = {
+    content: {
+        top:'50%',
+        left: '50%',
+        right: 'auto',
+        bottom: 'auto',
+        marginRight: '-50%',
+        transform: 'translate(-50%, -50%)'
+    }
+};
+
+Modal.setAppElement('')
+
 function Modal(){
+    let subtitle;
+    const [modalIsOpen, setIsOpen] = React.useState(false);
+
+    const openModal = () => {
+        setIsOpen(true)
+    }
+
+    const afterOpenModal = () => {
+        subtitle.style.color = '#f00';
+    }
+
+    const closeModal = () => {
+        setIsOpen(false);
+    }
+
     return(
-        alert(
-            <div>Modal</div>
-        )
+        <Modal
+        isOpen={modalIsOpen}
+        onAfterOpen={}
+        onRequestClose={}
+        style={}
+        contentLabel={}
+        >
+            <p>Employee Created!</p>
+        </Modal>
     )
 }
 
